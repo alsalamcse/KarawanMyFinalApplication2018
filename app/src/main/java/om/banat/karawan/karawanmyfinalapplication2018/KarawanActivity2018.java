023e94b51b59b8d0c91234996a257c18768f8fc3
jomanazene28 <jomana.zene1999@gmail.com>
2018-10-10 08:41:33 +0300

    Initial commit

---
+++ app/src/main/java/om/banat/karawan/karawanmyfinalapplication2018/KarawanActivity2018.java
@@ -0,0 +1,13 @@
+package om.banat.karawan.karawanmyfinalapplication2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KarawanActivity2018 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_karawan2018);
+    }
+}
