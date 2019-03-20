ec360bf2b8454d4070ecd1c5d32dcab125d797e8
karawan <banat811@gmail.com>
2019-03-20 10:46:21 +0200

    kk

--- app/src/main/java/om/banat/karawan/karawanmyfinalapplication2018/AddHajj.java
+++ app/src/main/java/om/banat/karawan/karawanmyfinalapplication2018/AddHajj.java
@@ -3,6 +3,7 @@ package om.banat.karawan.karawanmyfinalapplication2018;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,10 @@ import android.widget.EditText;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.jar.Attributes;
 
 public class AddHajj extends AppCompatActivity {
     private EditText edName;
@@ -20,12 +25,17 @@ public class AddHajj extends AppCompatActivity {
     private EditText edphone;
     private EditText edInformation;
     private Button btShare;
+    DatabaseReference databaseAddHajj;
+
+
 
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_hajj);
+        databaseAddHajj=FirebaseDatabase.getInstance().getReference("Hajj");
+
         edName = (EditText) findViewById(R.id.edName);
         edLastName = (EditText) findViewById(R.id.edLastName);
         edAge = (EditText) findViewById(R.id.edAge);
@@ -34,7 +44,10 @@ public class AddHajj extends AppCompatActivity {
         edphone = (EditText) findViewById(R.id.edphone);
         edInformation = (EditText) findViewById(R.id.edInformation);
         btShare = (Button) findViewById(R.id.btShare);
+       // auth=FirebaseAuth.getInstance();
+        //user=auth.getCurrentUser();
         btShare.setOnClickListener(new View.OnClickListener() {
+
 
             @Override
             public void onClick(View v) {
@@ -46,8 +59,8 @@ public class AddHajj extends AppCompatActivity {
     }
     private void dataHandler() {
 
-
-
+    }
+    private void AddHajj(){
         String Name =edName.getText().toString();
         String LastName=edLastName.getText().toString();
         String Age=edAge.getText().toString();
@@ -55,14 +68,27 @@ public class AddHajj extends AppCompatActivity {
         String Email=edEmail.getText().toString();
         String Phone=edphone.getText().toString();
         String Information=edInformation.getText().toString();
-    }
+        if (!TextUtils.isEmpty(Name+LastName+Age+PlaceLiving+Email+Phone+Information)){
 
-   // FirebaseAuth auth=FirebaseAuth.getInstance();
-    //
-
+        }
 
 
-}
+
+
+
+
+        }
+
+        }
+
+
+
+
+
+
+
+
+
 
 
 
--- app/src/main/java/om/banat/karawan/karawanmyfinalapplication2018/AddVolunteer.java
+++ app/src/main/java/om/banat/karawan/karawanmyfinalapplication2018/AddVolunteer.java
@@ -3,12 +3,16 @@ package om.banat.karawan.karawanmyfinalapplication2018;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddVolunteer extends AppCompatActivity {
 
@@ -22,6 +26,10 @@ public class AddVolunteer extends AppCompatActivity {
     private FirebaseUser user;
     private Button btvShare;
 
+    DatabaseReference databaseAddVolunteer;
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_volunteer);
@@ -34,8 +42,8 @@ public class AddVolunteer extends AppCompatActivity {
         btvShare=(Button) findViewById(R.id.btvShare);
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
-        FirebaseUser firebaseUser;
-        firebaseUser = auth.getCurrentUser();
+       // FirebaseUser firebaseUser;
+       // firebaseUser = auth.getCurrentUser();
 
         btvShare.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +61,14 @@ public class AddVolunteer extends AppCompatActivity {
         String PlaceLiving=etvPlaceLiving.getText().toString();
         String Phone=etvPhone.getText().toString();
         String Email=etvEmail.getText().toString();
+        if (!TextUtils.isEmpty(Name+LastName+Age+Phone+PlaceLiving+Email))
+        {
+            Toast.makeText(this,"Volunteer Added",Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this,"You Should enter a name",Toast.LENGTH_LONG).show();
+        }
+
 
 
 
